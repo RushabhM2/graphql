929d57dae6deef01dc2dd2c0132148c63b9d1701
Rushabh Mehta <rushabh.r.mehta@gmail.com>
2021-02-10 23:55:50 +0300

    FEAT: Added nested object types

--- client.js
+++ client.js
@@ -1,11 +1,19 @@
 /*
+const query = `query RollDice($dice, $sides) {
+    rollDice(numsides: $dice, numSides: $sides)
+}`
+// the $dice is the variable being passed to the resolver function
+
 fetch('/graphql', {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
   },
-  body: JSON.stringify({query: "{ hello }"})
+  body: JSON.stringify({
+      query: query, // query is specified above
+      variables: {dice: 3, sides: 6}
+  })
 })
   .then(r => r.json())
   .then(data => console.log('data returned:', data));
--- server.js
+++ server.js
@@ -1,21 +1,51 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
-const { graphql, buildSchema } = require('graphql');
+const { graphql, buildSchema, isOutputType } = require('graphql');
 const app = express();
 
 // use the buildSchema to create schemas for each query
 // Inside Query we have the name of the query/mutation endpoint (hello) and the type of return expected
+// rollDice is a query that takes in 2 parameters, one is mandatory
 const schema = buildSchema(`
+    type RandomDie {
+        numSides: Int!
+        rollOnce: Int!
+        roll(numRolls: Int!): [Int]
+    }
+
     type Query {
-        quoteOfTheDay: String
-        random: Float!
-        rollThreeDice: [Int]
+        getDie(numSides: Int!): RandomDie
     }
 `);
 
+/* 
+        quoteOfTheDay: String
+        random: Float!
+        rollThreeDice: [Int]
+        rollDice(numDice: Int!, numSides: Int): [Int]
+*/
+
+class RandomDie {
+    constructor(numSides) {
+        this.numSides = numSides
+    }
+
+    rollOnce() {
+        return 1+Math.floor(Math.random() * this.numSides)
+    }
+
+    roll({numRolls}) {
+        let output = [];
+        for (let i=0; i<numRolls; i++) {
+            output.push(this.rollOnce())
+        }
+        return output;
+    }
+}
+
 // There is a resolver for each query schema above
 const root = {
-    quoteOfTheDay: ()=>{
+    /*quoteOfTheDay: ()=>{
         return Math.random() < 0.5 ? 'Hello world' : 'other quote'
     },
     random: ()=>{
@@ -23,6 +53,21 @@ const root = {
     },
     rollThreeDice: ()=>{
         return [1,2,3].map(_ => 1+Math.floor(Math.random()*6)) // This function returns an array of three numbers between 1 and 6 - representing the score on each die
+    },
+    rollDice: (args)=>{
+        // This function returns an array the length of numDice (a number for each die rolled)
+        // Each die value will default to 6, unless numSides is specified
+
+        // This function can be written with parameters,
+        // We can also destructure the arguments to {numDice, numSides}
+        let output = [];
+        for (let i=0; i<args.numDice; i++) {
+            output.push(1+Math.floor(Math.random()*(args.numSides || 6))) 
+        }
+        return output;
+    },*/
+    getDie: ({numSides}) => {
+        return new RandomDie(numSides || 6)
     }
 }
 
