6844cd68e1912b2d7f67e73b926520c5fb8f92de
Rushabh Mehta <rushabh.r.mehta@gmail.com>
2021-02-10 17:48:15 +0300

    FEAT: Express server added

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
 const { graphql, buildSchema } = require('graphql');
+const app = express();
 
 // use the buildSchema to create schemas for each query
 // Inside Query we have the name of the query/mutation endpoint (hello) and the type of return expected
@@ -15,6 +18,13 @@ const root = {
     }
 }
 
-graphql(schema, '{hello}', root).then((response)=>{
-    console.log(response)
+app.use('/graphql', graphqlHTTP({
+    schema: schema, // Schema defined above
+    rootValue: root, // Root resolver defined above
+    graphiql: true // Allowing access in the graphql playground
+}))
+
+app.listen(4001, ()=>{
+    console.log('Server listening on http://localhost:4001')
 })
+console.log('Running a GraphQL API server at http://localhost:4001/graphql');
